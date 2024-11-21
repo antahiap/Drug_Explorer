c97b534725e2457f02f86c8a076dbc6439a47486
anahita <antahia@yahoo.com>
2024-11-21 16:43:01 +0000

    run demo on aws

--- src/Const.ts
+++ src/Const.ts
@@ -1,9 +1,10 @@
+const HOST = '172.29.45.53';
 const STATIC_URL =
   process.env.NODE_ENV === 'development'
-    ? `http://localhost:${process.env.REACT_APP_PORT}`
+    ? `http://${HOST}:${process.env.REACT_APP_PORT}`
     : './';
 
 const SERVER_URL =
-  process.env.NODE_ENV === 'development' ? `http://localhost:8002` : './';
+  process.env.NODE_ENV === 'development' ? `http://${HOST}:8002` : './';
 
 export { STATIC_URL, SERVER_URL };
